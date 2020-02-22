29130c4818414d3170b1cf91391e1a38a7579877
Florian <florian.rieder@unil.ch>
2020-02-22 18:35:58 +0100

    Added terrain

---
+++ Orbital3/Assets/TerrainScroller.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainScroller : MonoBehaviour
+{
+    [SerializeField] private float scrollSpeed = .5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(-1,0,0) * scrollSpeed * Time.deltaTime;
+    }
+}
