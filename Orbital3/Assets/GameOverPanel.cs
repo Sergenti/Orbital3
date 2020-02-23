84262cccab5c8a7dcc003be87d1dd2c20d8f8c99
Florian <florian.rieder@unil.ch>
2020-02-23 04:56:06 +0100

    Created Game Over Panel

---
+++ Orbital3/Assets/GameOverPanel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameOverPanel : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI playerName;
+    public void SetWinner(string winner, Color color)
+    {
+        playerName.text = winner + "'s";
+        playerName.color = color;
+    }
+}
