8add84270a0712ef6d8ac1e571bd96bd3b4a3149
Pedro.Tome <tome.pedro.sierre@gmail.com>
2020-02-23 12:28:41 +0100

    Update Footsteps.cs

--- Orbital3/Assets/Audio/Scripts/Footsteps.cs
+++ Orbital3/Assets/Audio/Scripts/Footsteps.cs
@@ -14,23 +14,23 @@ public class Footsteps : MonoBehaviour
     [SerializeField] private AudioClip ftsp7;
     [SerializeField] private AudioClip ftsp8;
     void Start() {
-         AudioSource audio = GetComponent<AudioSource>();
+         
     }
     public void PlayFootsteps(Vector2 vector, PlayerStats stats){
         bool isNull = vector.magnitude < 0.1;
-        
+        AudioSource audio = GetComponent<AudioSource>();
         switch(stats.weightLevelIdx){
-            case 0: if (isNull){if (!GetComponent<AudioSource>().isPlaying){GetComponent<AudioSource>().Play();}}
-                    else {GetComponent<AudioSource>().clip = ftsp2; if(!GetComponent<AudioSource>().isPlaying){GetComponent<AudioSource>().Play();}}
+            case 0: if (isNull){if (!audio.isPlaying){audio.Play();}}
+                    else {audio.Stop(); audio.clip = ftsp2; if(!audio.isPlaying){audio.Play();}}
                     break;
-            case 1: if (isNull){GetComponent<AudioSource>().clip = ftsp3;if(!GetComponent<AudioSource>().isPlaying){GetComponent<AudioSource>().Play();}}
-                    else {GetComponent<AudioSource>().clip = ftsp4; if(!GetComponent<AudioSource>().isPlaying){GetComponent<AudioSource>().Play();}}
+            case 1: if (isNull){audio.clip = ftsp3;if(!audio.isPlaying){audio.Play();}}
+                    else {audio.clip = ftsp4; if(!audio.isPlaying){audio.Play();}}
                     break;
-            case 2: if (isNull){GetComponent<AudioSource>().clip = ftsp5; if(!GetComponent<AudioSource>().isPlaying){GetComponent<AudioSource>().Play();}}
-                    else {GetComponent<AudioSource>().clip = ftsp6; if(!GetComponent<AudioSource>().isPlaying){GetComponent<AudioSource>().Play();}}
+            case 2: if (isNull){audio.clip = ftsp5; if(!audio.isPlaying){audio.Play();}}
+                    else {audio.clip = ftsp6; if(!audio.isPlaying){audio.Play();}}
                     break;
-            case 3: if(isNull){GetComponent<AudioSource>().clip = ftsp7;if(!GetComponent<AudioSource>().isPlaying){GetComponent<AudioSource>().Play();}}
-                    else {GetComponent<AudioSource>().clip = ftsp8; if(!GetComponent<AudioSource>().isPlaying){GetComponent<AudioSource>().Play();}}
+            case 3: if(isNull){audio.clip = ftsp7;if(!audio.isPlaying){audio.Play();}}
+                    else {audio.clip = ftsp8; if(!audio.isPlaying){audio.Play();}}
                     break;
         }
     }
