7f271f8b48c4fedeb63ead31557ab92774ab5117
Florian <florian.rieder@unil.ch>
2020-02-23 06:41:30 +0100

    game over panel

--- Orbital3/Assets/GameOverPanel.cs
+++ Orbital3/Assets/GameOverPanel.cs
@@ -1,14 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using TMPro;
 
 public class GameOverPanel : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI playerName;
+    [SerializeField] private GameObject firstSelectedButton;
     public void SetWinner(string winner, Color color)
     {
         playerName.text = winner + "'s";
         playerName.color = color;
+    }
+
+    public void Enable(bool value)
+    {
+        gameObject.SetActive(value);
+
+        // continue only if we are opening the panel
+        if(!value) return; 
+
+        // select the first button, for gamepad navigation of the UI
+        if (!EventSystem.current.alreadySelecting)
+        {
+            EventSystem.current.SetSelectedGameObject(firstSelectedButton);
+        }
     }
 }
